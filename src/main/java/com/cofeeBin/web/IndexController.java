823077dc37e753aaef1c531fffdf756955530a88
philos1234 <48795102+philos1234@users.noreply.github.com>
2021-09-28 20:18:06 +0900

    indexController

---
+++ src/main/java/com/cofeeBin/web/IndexController.java
@@ -0,0 +1,16 @@
+package com.cofeeBin.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(){
+
+        return "index";
+    }
+
+}
